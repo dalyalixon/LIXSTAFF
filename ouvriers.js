e18a5c1ebc2f8a7edb6686fa593a5468ac61300b
dalyalixon <d.hamache@lixon.net>
2025-10-06 10:36:19 +0200

    Add ouvriers.js (liste ouvriers depuis Excel)

---
+++ ouvriers.js
@@ -0,0 +1,573 @@
+/* Auto-generated from TABLEAU METIER EVALUATIONS.xlsx */
+const OUVRIERS = [
+  {
+    "matricule": "736",
+    "nom": "VANNEMBERG",
+    "prenom": "JACQUES",
+    "entree": "1984-05-24",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "892",
+    "nom": "ROLAIN",
+    "prenom": "DIDIER",
+    "entree": "1987-02-17",
+    "qualif": "9",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1032",
+    "nom": "HODY",
+    "prenom": "ALAIN",
+    "entree": "1989-08-28",
+    "qualif": "4",
+    "fonction": "Maintenance"
+  },
+  {
+    "matricule": "1114",
+    "nom": "HOEBEKE",
+    "prenom": "FABIAN",
+    "entree": "1990-10-16",
+    "qualif": "7",
+    "fonction": "Ferrailleurs"
+  },
+  {
+    "matricule": "1177",
+    "nom": "WATHELET",
+    "prenom": "BERNARD",
+    "entree": "1992-01-06",
+    "qualif": "7",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1227",
+    "nom": "FIORINO",
+    "prenom": "ROCCO",
+    "entree": "1992-05-11",
+    "qualif": "4",
+    "fonction": "Grutiers"
+  },
+  {
+    "matricule": "1334",
+    "nom": "SELASSI",
+    "prenom": "YOUSSEF",
+    "entree": "1993-10-08",
+    "qualif": "4",
+    "fonction": "Maintenance"
+  },
+  {
+    "matricule": "1387",
+    "nom": "POTIER",
+    "prenom": "WILLIAM",
+    "entree": "1997-06-16",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1410",
+    "nom": "LECLERCQ",
+    "prenom": "ROMUALD",
+    "entree": "1998-02-19",
+    "qualif": "4",
+    "fonction": "Grutiers"
+  },
+  {
+    "matricule": "1450",
+    "nom": "AYACHE",
+    "prenom": "ALI",
+    "entree": "1998-09-01",
+    "qualif": "7",
+    "fonction": "Electromécaniciens"
+  },
+  {
+    "matricule": "1469",
+    "nom": "PECCIO",
+    "prenom": "GIACOMO",
+    "entree": "1998-10-01",
+    "qualif": "3",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1495",
+    "nom": "NOR",
+    "prenom": "MOHAMED",
+    "entree": "1999-09-20",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1505",
+    "nom": "DREZE",
+    "prenom": "DOMINIQUE",
+    "entree": "2000-01-17",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1528",
+    "nom": "CUCU",
+    "prenom": "FABIAN",
+    "entree": "2001-08-08",
+    "qualif": "7",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1540",
+    "nom": "BAUFAYS",
+    "prenom": "BRUNO",
+    "entree": "2002-10-14",
+    "qualif": "4",
+    "fonction": "Ferrailleurs"
+  },
+  {
+    "matricule": "1567",
+    "nom": "GIANA",
+    "prenom": "FRANCO",
+    "entree": "2004-09-06",
+    "qualif": "3",
+    "fonction": "Electricien"
+  },
+  {
+    "matricule": "1576",
+    "nom": "LELOIR",
+    "prenom": "JEAN-PIERRE",
+    "entree": "2005-10-03",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1591",
+    "nom": "WAUTELET",
+    "prenom": "PIERRE",
+    "entree": "2006-08-01",
+    "qualif": "8",
+    "fonction": "Electromécaniciens"
+  },
+  {
+    "matricule": "1630",
+    "nom": "TIRRITO",
+    "prenom": "IGNAZIO",
+    "entree": "2009-09-02",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1641",
+    "nom": "ADESIONE",
+    "prenom": "JULIEN",
+    "entree": "2010-11-17",
+    "qualif": "7",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1643",
+    "nom": "FAINI",
+    "prenom": "CHRISTOPHE",
+    "entree": "2010-11-22",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1655",
+    "nom": "MANNINO",
+    "prenom": "LIVIO",
+    "entree": "2011-04-04",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1657",
+    "nom": "SCHIAVONE",
+    "prenom": "MICHELE",
+    "entree": "2011-05-10",
+    "qualif": "8",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1658",
+    "nom": "GONZALEZ-ALEGRE",
+    "prenom": "VINCENT",
+    "entree": "2011-05-16",
+    "qualif": "8",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1670",
+    "nom": "LO CICERO",
+    "prenom": "VINCENZO",
+    "entree": "2013-10-14",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1673",
+    "nom": "HAINE",
+    "prenom": "QUENTIN",
+    "entree": "2014-09-08",
+    "qualif": "3",
+    "fonction": "Soudeur "
+  },
+  {
+    "matricule": "1675",
+    "nom": "PASQUALINO",
+    "prenom": "ANTONINO",
+    "entree": "2016-12-01",
+    "qualif": "9",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1682",
+    "nom": "ANGOTZI",
+    "prenom": "VICTORIO",
+    "entree": "2018-06-04",
+    "qualif": "7",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1686",
+    "nom": "MORANDI",
+    "prenom": "GIANNI",
+    "entree": "2019-08-15",
+    "qualif": "9",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1691",
+    "nom": "KESKIN",
+    "prenom": "AHMET",
+    "entree": "2019-10-01",
+    "qualif": "5",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1693",
+    "nom": "CLAUS",
+    "prenom": "CORENTHIN",
+    "entree": "2019-10-01",
+    "qualif": "2",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1696",
+    "nom": "CIMINO",
+    "prenom": "MANUELE",
+    "entree": "2020-08-04",
+    "qualif": "3",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1697",
+    "nom": "OCCHICONE",
+    "prenom": "NICOLA",
+    "entree": "2020-08-04",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1698",
+    "nom": "SPANU",
+    "prenom": "YOURI",
+    "entree": "2020-08-04",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1700",
+    "nom": "BOONE",
+    "prenom": "DIMITRI",
+    "entree": "2020-10-26",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1701",
+    "nom": "LICATA",
+    "prenom": "LUIGINO",
+    "entree": "2021-01-04",
+    "qualif": "7",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1703",
+    "nom": "TALMAT",
+    "prenom": "ALOIS",
+    "entree": "2021-06-01",
+    "qualif": "3",
+    "fonction": "Electricien"
+  },
+  {
+    "matricule": "1707",
+    "nom": "LIVORNESE",
+    "prenom": "FLORENT",
+    "entree": "2021-10-04",
+    "qualif": "8",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1710",
+    "nom": "HENNAUT",
+    "prenom": "JESSY",
+    "entree": "2022-04-11",
+    "qualif": "3",
+    "fonction": "Electricien"
+  },
+  {
+    "matricule": "1712",
+    "nom": "BUISSERET",
+    "prenom": "PATRICE",
+    "entree": "2022-08-09",
+    "qualif": "8",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1715",
+    "nom": "SCAILLET",
+    "prenom": "LUCAS",
+    "entree": "2022-08-09",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1716",
+    "nom": "MONTREUIL",
+    "prenom": "THOMAS",
+    "entree": "2022-11-22",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1718",
+    "nom": "MORANDI",
+    "prenom": "FABIO",
+    "entree": "2022-11-21",
+    "qualif": "3",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1720",
+    "nom": "VETRI",
+    "prenom": "ENRICO",
+    "entree": "2023-03-01",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1722",
+    "nom": "VALET",
+    "prenom": "LOGAN",
+    "entree": "2023-05-02",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1723",
+    "nom": "DJEUDEU",
+    "prenom": "JIRESSE",
+    "entree": "2023-08-08",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1725",
+    "nom": "KAMDOUM",
+    "prenom": "TIERRY TCHEATCHUI",
+    "entree": "2023-08-08",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1726",
+    "nom": "DEVERGNIES",
+    "prenom": "FRÉDÉRIC",
+    "entree": "2023-08-21",
+    "qualif": "4",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1734",
+    "nom": "KILFIGER",
+    "prenom": "CHRISTOPHE",
+    "entree": "2024-03-04",
+    "qualif": "3",
+    "fonction": "Chauffeurs"
+  },
+  {
+    "matricule": "1735",
+    "nom": "LERAT",
+    "prenom": "SEBASTIEN",
+    "entree": "2024-03-25",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1736",
+    "nom": "MENDOLA",
+    "prenom": "GINO",
+    "entree": "2024-04-15",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1737",
+    "nom": "MAGAL",
+    "prenom": "ALEXANDRE",
+    "entree": "2024-04-15",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1738",
+    "nom": "VENTURUZZO",
+    "prenom": "JULIEN",
+    "entree": "2024-04-15",
+    "qualif": "3",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1739",
+    "nom": "DUTIENNE",
+    "prenom": "JEREMY",
+    "entree": "2024-04-22",
+    "qualif": "3",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1740",
+    "nom": "DUTIENNE",
+    "prenom": "NICOLAS",
+    "entree": "2024-04-22",
+    "qualif": "5",
+    "fonction": "Coffreurs"
+  },
+  {
+    "matricule": "1741",
+    "nom": "LERAT",
+    "prenom": "FABIEN",
+    "entree": "2024-05-28",
+    "qualif": "4",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1743",
+    "nom": "VANRILLAER",
+    "prenom": "JASON",
+    "entree": "2024-08-05",
+    "qualif": "1",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1745",
+    "nom": "LECLERCQ",
+    "prenom": "MAXIME",
+    "entree": "2025-02-10",
+    "qualif": "1",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1746",
+    "nom": "LIENARD",
+    "prenom": "JONATHAN",
+    "entree": "2025-02-10",
+    "qualif": "5",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1747",
+    "nom": "LAENEN",
+    "prenom": "KEVIN",
+    "entree": "2025-02-10",
+    "qualif": "1",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1749",
+    "nom": "DELCOURT",
+    "prenom": "VINCENT",
+    "entree": "2025-02-11",
+    "qualif": "6",
+    "fonction": "Grutiers"
+  },
+  {
+    "matricule": "1750",
+    "nom": "PARFONRY",
+    "prenom": "THIBAUT",
+    "entree": "2025-03-19",
+    "qualif": "6",
+    "fonction": "Electricien"
+  },
+  {
+    "matricule": "1751",
+    "nom": "POLIMENI",
+    "prenom": "GIOVANNI",
+    "entree": "2025-03-19",
+    "qualif": "6",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1752",
+    "nom": "LARIVERA",
+    "prenom": "THOMAS",
+    "entree": "2025-04-07",
+    "qualif": "3",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1753",
+    "nom": "HASSELIN",
+    "prenom": "LEO",
+    "entree": "2025-04-07",
+    "qualif": "5",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1754",
+    "nom": "DEMEZEL",
+    "prenom": "MASSIMO",
+    "entree": "2025-05-05",
+    "qualif": "5",
+    "fonction": "Maintenance"
+  },
+  {
+    "matricule": "1755",
+    "nom": "DE KLERCK",
+    "prenom": "ADRIEN",
+    "entree": "2025-08-05",
+    "qualif": "99",
+    "fonction": "PFI"
+  },
+  {
+    "matricule": "1756",
+    "nom": "BAMBISI",
+    "prenom": "JEANCY",
+    "entree": "2025-08-05",
+    "qualif": "99",
+    "fonction": "PFI"
+  },
+  {
+    "matricule": "1757",
+    "nom": "VILLAREALE",
+    "prenom": "ROMAIN",
+    "entree": "2025-09-01",
+    "qualif": "2",
+    "fonction": "Chauffeurs"
+  },
+  {
+    "matricule": "1758",
+    "nom": "KUBAN",
+    "prenom": "ADRIEN",
+    "entree": "2025-09-22",
+    "qualif": "3",
+    "fonction": "Maçons"
+  },
+  {
+    "matricule": "1759",
+    "nom": "HULIN",
+    "prenom": "SEBASTIEN",
+    "entree": "2025-10-01",
+    "qualif": "3",
+    "fonction": "Maçons"
+  }
+];
+if (typeof window !== 'undefined') { window.OUVRIERS = OUVRIERS; }
+export { OUVRIERS };
