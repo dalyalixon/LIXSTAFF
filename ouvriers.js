096945bdc300249927805e24216865335d9c87b4
dalyalixon <d.hamache@lixon.net>
2025-10-06 11:17:57 +0200

    Update ouvriers.js

--- ouvriers.js
+++ ouvriers.js
@@ -4,6 +4,7 @@ window.OUVRIERS = [
     "matricule": "736",
     "nom": "VANNEMBERG",
     "prenom": "JACQUES",
+    "naissance": "1964-09-25",
     "entree": "1984-05-24",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -12,6 +13,7 @@ window.OUVRIERS = [
     "matricule": "892",
     "nom": "ROLAIN",
     "prenom": "DIDIER",
+    "naissance": "1963-06-09",
     "entree": "1987-02-17",
     "qualif": "9",
     "fonction": "Maçons"
@@ -20,6 +22,7 @@ window.OUVRIERS = [
     "matricule": "1032",
     "nom": "HODY",
     "prenom": "ALAIN",
+    "naissance": "1969-02-22",
     "entree": "1989-08-28",
     "qualif": "4",
     "fonction": "Maintenance"
@@ -28,6 +31,7 @@ window.OUVRIERS = [
     "matricule": "1114",
     "nom": "HOEBEKE",
     "prenom": "FABIAN",
+    "naissance": "1964-11-19",
     "entree": "1990-10-16",
     "qualif": "7",
     "fonction": "Ferrailleurs"
@@ -36,6 +40,7 @@ window.OUVRIERS = [
     "matricule": "1177",
     "nom": "WATHELET",
     "prenom": "BERNARD",
+    "naissance": "1965-12-21",
     "entree": "1992-01-06",
     "qualif": "7",
     "fonction": "Maçons"
@@ -44,6 +49,7 @@ window.OUVRIERS = [
     "matricule": "1227",
     "nom": "FIORINO",
     "prenom": "ROCCO",
+    "naissance": "1965-12-02",
     "entree": "1992-05-11",
     "qualif": "4",
     "fonction": "Grutiers"
@@ -52,6 +58,7 @@ window.OUVRIERS = [
     "matricule": "1334",
     "nom": "SELASSI",
     "prenom": "YOUSSEF",
+    "naissance": "1969-12-03",
     "entree": "1993-10-08",
     "qualif": "4",
     "fonction": "Maintenance"
@@ -60,6 +67,7 @@ window.OUVRIERS = [
     "matricule": "1387",
     "nom": "POTIER",
     "prenom": "WILLIAM",
+    "naissance": "1964-06-02",
     "entree": "1997-06-16",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -68,6 +76,7 @@ window.OUVRIERS = [
     "matricule": "1410",
     "nom": "LECLERCQ",
     "prenom": "ROMUALD",
+    "naissance": "1971-03-31",
     "entree": "1998-02-19",
     "qualif": "4",
     "fonction": "Grutiers"
@@ -76,6 +85,7 @@ window.OUVRIERS = [
     "matricule": "1450",
     "nom": "AYACHE",
     "prenom": "ALI",
+    "naissance": "1961-08-21",
     "entree": "1998-09-01",
     "qualif": "7",
     "fonction": "Electromécaniciens"
@@ -84,6 +94,7 @@ window.OUVRIERS = [
     "matricule": "1469",
     "nom": "PECCIO",
     "prenom": "GIACOMO",
+    "naissance": "1966-07-22",
     "entree": "1998-10-01",
     "qualif": "3",
     "fonction": "Maçons"
@@ -92,6 +103,7 @@ window.OUVRIERS = [
     "matricule": "1495",
     "nom": "NOR",
     "prenom": "MOHAMED",
+    "naissance": "1961-07-25",
     "entree": "1999-09-20",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -100,6 +112,7 @@ window.OUVRIERS = [
     "matricule": "1505",
     "nom": "DREZE",
     "prenom": "DOMINIQUE",
+    "naissance": "1971-02-22",
     "entree": "2000-01-17",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -108,6 +121,7 @@ window.OUVRIERS = [
     "matricule": "1528",
     "nom": "CUCU",
     "prenom": "FABIAN",
+    "naissance": "1967-11-12",
     "entree": "2001-08-08",
     "qualif": "7",
     "fonction": "Coffreurs"
@@ -116,6 +130,7 @@ window.OUVRIERS = [
     "matricule": "1540",
     "nom": "BAUFAYS",
     "prenom": "BRUNO",
+    "naissance": "1983-04-06",
     "entree": "2002-10-14",
     "qualif": "4",
     "fonction": "Ferrailleurs"
@@ -124,6 +139,7 @@ window.OUVRIERS = [
     "matricule": "1567",
     "nom": "GIANA",
     "prenom": "FRANCO",
+    "naissance": "1969-05-12",
     "entree": "2004-09-06",
     "qualif": "3",
     "fonction": "Electricien"
@@ -132,6 +148,7 @@ window.OUVRIERS = [
     "matricule": "1576",
     "nom": "LELOIR",
     "prenom": "JEAN-PIERRE",
+    "naissance": "1976-09-01",
     "entree": "2005-10-03",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -140,6 +157,7 @@ window.OUVRIERS = [
     "matricule": "1591",
     "nom": "WAUTELET",
     "prenom": "PIERRE",
+    "naissance": "1981-12-28",
     "entree": "2006-08-01",
     "qualif": "8",
     "fonction": "Electromécaniciens"
@@ -148,6 +166,7 @@ window.OUVRIERS = [
     "matricule": "1630",
     "nom": "TIRRITO",
     "prenom": "IGNAZIO",
+    "naissance": "1990-09-26",
     "entree": "2009-09-02",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -156,6 +175,7 @@ window.OUVRIERS = [
     "matricule": "1641",
     "nom": "ADESIONE",
     "prenom": "JULIEN",
+    "naissance": "1986-04-20",
     "entree": "2010-11-17",
     "qualif": "7",
     "fonction": "Coffreurs"
@@ -164,6 +184,7 @@ window.OUVRIERS = [
     "matricule": "1643",
     "nom": "FAINI",
     "prenom": "CHRISTOPHE",
+    "naissance": "1979-10-10",
     "entree": "2010-11-22",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -172,6 +193,7 @@ window.OUVRIERS = [
     "matricule": "1655",
     "nom": "MANNINO",
     "prenom": "LIVIO",
+    "naissance": "1990-11-14",
     "entree": "2011-04-04",
     "qualif": "4",
     "fonction": "Maçons"
@@ -180,6 +202,7 @@ window.OUVRIERS = [
     "matricule": "1657",
     "nom": "SCHIAVONE",
     "prenom": "MICHELE",
+    "naissance": "1971-06-02",
     "entree": "2011-05-10",
     "qualif": "8",
     "fonction": "Maçons"
@@ -188,6 +211,7 @@ window.OUVRIERS = [
     "matricule": "1658",
     "nom": "GONZALEZ-ALEGRE",
     "prenom": "VINCENT",
+    "naissance": "1986-11-15",
     "entree": "2011-05-16",
     "qualif": "8",
     "fonction": "Coffreurs"
@@ -196,6 +220,7 @@ window.OUVRIERS = [
     "matricule": "1670",
     "nom": "LO CICERO",
     "prenom": "VINCENZO",
+    "naissance": "1968-07-20",
     "entree": "2013-10-14",
     "qualif": "4",
     "fonction": "Maçons"
@@ -204,6 +229,7 @@ window.OUVRIERS = [
     "matricule": "1673",
     "nom": "HAINE",
     "prenom": "QUENTIN",
+    "naissance": "1988-07-28",
     "entree": "2014-09-08",
     "qualif": "3",
     "fonction": "Soudeur"
@@ -212,6 +238,7 @@ window.OUVRIERS = [
     "matricule": "1675",
     "nom": "PASQUALINO",
     "prenom": "ANTONINO",
+    "naissance": "1989-11-19",
     "entree": "2016-12-01",
     "qualif": "9",
     "fonction": "Maçons"
@@ -220,6 +247,7 @@ window.OUVRIERS = [
     "matricule": "1682",
     "nom": "ANGOTZI",
     "prenom": "VICTORIO",
+    "naissance": "1970-01-15",
     "entree": "2018-06-04",
     "qualif": "7",
     "fonction": "Coffreurs"
@@ -228,6 +256,7 @@ window.OUVRIERS = [
     "matricule": "1686",
     "nom": "MORANDI",
     "prenom": "GIANNI",
+    "naissance": "1985-11-03",
     "entree": "2019-08-15",
     "qualif": "9",
     "fonction": "Coffreurs"
@@ -236,6 +265,7 @@ window.OUVRIERS = [
     "matricule": "1691",
     "nom": "KESKIN",
     "prenom": "AHMET",
+    "naissance": "1993-04-17",
     "entree": "2019-10-01",
     "qualif": "5",
     "fonction": "Maçons"
@@ -244,6 +274,7 @@ window.OUVRIERS = [
     "matricule": "1693",
     "nom": "CLAUS",
     "prenom": "CORENTHIN",
+    "naissance": "1997-01-14",
     "entree": "2019-10-01",
     "qualif": "2",
     "fonction": "Maçons"
@@ -252,6 +283,7 @@ window.OUVRIERS = [
     "matricule": "1696",
     "nom": "CIMINO",
     "prenom": "MANUELE",
+    "naissance": "1979-05-01",
     "entree": "2020-08-04",
     "qualif": "3",
     "fonction": "Coffreurs"
@@ -260,6 +292,7 @@ window.OUVRIERS = [
     "matricule": "1697",
     "nom": "OCCHICONE",
     "prenom": "NICOLA",
+    "naissance": "1992-06-06",
     "entree": "2020-08-04",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -268,6 +301,7 @@ window.OUVRIERS = [
     "matricule": "1698",
     "nom": "SPANU",
     "prenom": "YOURI",
+    "naissance": "1990-02-14",
     "entree": "2020-08-04",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -276,6 +310,7 @@ window.OUVRIERS = [
     "matricule": "1700",
     "nom": "BOONE",
     "prenom": "DIMITRI",
+    "naissance": "1980-02-03",
     "entree": "2020-10-26",
     "qualif": "4",
     "fonction": "Maçons"
@@ -284,6 +319,7 @@ window.OUVRIERS = [
     "matricule": "1701",
     "nom": "LICATA",
     "prenom": "LUIGINO",
+    "naissance": "1977-12-06",
     "entree": "2021-01-04",
     "qualif": "7",
     "fonction": "Maçons"
@@ -292,6 +328,7 @@ window.OUVRIERS = [
     "matricule": "1703",
     "nom": "TALMAT",
     "prenom": "ALOIS",
+    "naissance": "1990-05-18",
     "entree": "2021-06-01",
     "qualif": "3",
     "fonction": "Electricien"
@@ -300,6 +337,7 @@ window.OUVRIERS = [
     "matricule": "1707",
     "nom": "LIVORNESE",
     "prenom": "FLORENT",
+    "naissance": "1988-12-24",
     "entree": "2021-10-04",
     "qualif": "8",
     "fonction": "Maçons"
@@ -308,6 +346,7 @@ window.OUVRIERS = [
     "matricule": "1710",
     "nom": "HENNAUT",
     "prenom": "JESSY",
+    "naissance": "1991-03-07",
     "entree": "2022-04-11",
     "qualif": "3",
     "fonction": "Electricien"
@@ -316,6 +355,7 @@ window.OUVRIERS = [
     "matricule": "1712",
     "nom": "BUISSERET",
     "prenom": "PATRICE",
+    "naissance": "1984-12-17",
     "entree": "2022-08-09",
     "qualif": "8",
     "fonction": "Coffreurs"
@@ -324,6 +364,7 @@ window.OUVRIERS = [
     "matricule": "1715",
     "nom": "SCAILLET",
     "prenom": "LUCAS",
+    "naissance": "2001-03-10",
     "entree": "2022-08-09",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -332,6 +373,7 @@ window.OUVRIERS = [
     "matricule": "1716",
     "nom": "MONTREUIL",
     "prenom": "THOMAS",
+    "naissance": "1997-11-15",
     "entree": "2022-11-22",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -340,6 +382,7 @@ window.OUVRIERS = [
     "matricule": "1718",
     "nom": "MORANDI",
     "prenom": "FABIO",
+    "naissance": "1982-12-02",
     "entree": "2022-11-21",
     "qualif": "3",
     "fonction": "Coffreurs"
@@ -348,6 +391,7 @@ window.OUVRIERS = [
     "matricule": "1720",
     "nom": "VETRI",
     "prenom": "ENRICO",
+    "naissance": "1986-05-25",
     "entree": "2023-03-01",
     "qualif": "4",
     "fonction": "Maçons"
@@ -356,6 +400,7 @@ window.OUVRIERS = [
     "matricule": "1722",
     "nom": "VALET",
     "prenom": "LOGAN",
+    "naissance": "1990-07-31",
     "entree": "2023-05-02",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -364,6 +409,7 @@ window.OUVRIERS = [
     "matricule": "1723",
     "nom": "DJEUDEU",
     "prenom": "JIRESSE",
+    "naissance": "1991-02-15",
     "entree": "2023-08-08",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -372,6 +418,7 @@ window.OUVRIERS = [
     "matricule": "1725",
     "nom": "KAMDOUM",
     "prenom": "TIERRY TCHEATCHUI",
+    "naissance": "1988-07-17",
     "entree": "2023-08-08",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -380,6 +427,7 @@ window.OUVRIERS = [
     "matricule": "1726",
     "nom": "DEVERGNIES",
     "prenom": "FRÉDÉRIC",
+    "naissance": "1977-01-23",
     "entree": "2023-08-21",
     "qualif": "4",
     "fonction": "Coffreurs"
@@ -388,6 +436,7 @@ window.OUVRIERS = [
     "matricule": "1734",
     "nom": "KILFIGER",
     "prenom": "CHRISTOPHE",
+    "naissance": "1974-07-07",
     "entree": "2024-03-04",
     "qualif": "3",
     "fonction": "Chauffeurs"
@@ -396,6 +445,7 @@ window.OUVRIERS = [
     "matricule": "1735",
     "nom": "LERAT",
     "prenom": "SEBASTIEN",
+    "naissance": "1982-06-12",
     "entree": "2024-03-25",
     "qualif": "4",
     "fonction": "Maçons"
@@ -404,6 +454,7 @@ window.OUVRIERS = [
     "matricule": "1736",
     "nom": "MENDOLA",
     "prenom": "GINO",
+    "naissance": "1984-10-22",
     "entree": "2024-04-15",
     "qualif": "4",
     "fonction": "Maçons"
@@ -412,6 +463,7 @@ window.OUVRIERS = [
     "matricule": "1737",
     "nom": "MAGAL",
     "prenom": "ALEXANDRE",
+    "naissance": "1986-09-06",
     "entree": "2024-04-15",
     "qualif": "4",
     "fonction": "Maçons"
@@ -420,6 +472,7 @@ window.OUVRIERS = [
     "matricule": "1738",
     "nom": "VENTURUZZO",
     "prenom": "JULIEN",
+    "naissance": "1988-07-15",
     "entree": "2024-04-15",
     "qualif": "3",
     "fonction": "Maçons"
@@ -428,6 +481,7 @@ window.OUVRIERS = [
     "matricule": "1739",
     "nom": "DUTIENNE",
     "prenom": "JEREMY",
+    "naissance": "1981-01-28",
     "entree": "2024-04-22",
     "qualif": "3",
     "fonction": "Coffreurs"
@@ -436,6 +490,7 @@ window.OUVRIERS = [
     "matricule": "1740",
     "nom": "DUTIENNE",
     "prenom": "NICOLAS",
+    "naissance": "1989-01-27",
     "entree": "2024-04-22",
     "qualif": "5",
     "fonction": "Coffreurs"
@@ -444,6 +499,7 @@ window.OUVRIERS = [
     "matricule": "1741",
     "nom": "LERAT",
     "prenom": "FABIEN",
+    "naissance": "1982-06-12",
     "entree": "2024-05-28",
     "qualif": "4",
     "fonction": "Maçons"
@@ -452,6 +508,7 @@ window.OUVRIERS = [
     "matricule": "1743",
     "nom": "VANRILLAER",
     "prenom": "JASON",
+    "naissance": "2004-11-23",
     "entree": "2024-08-05",
     "qualif": "1",
     "fonction": "Maçons"
@@ -460,6 +517,7 @@ window.OUVRIERS = [
     "matricule": "1745",
     "nom": "LECLERCQ",
     "prenom": "MAXIME",
+    "naissance": "2003-03-24",
     "entree": "2025-02-10",
     "qualif": "1",
     "fonction": "Maçons"
@@ -468,6 +526,7 @@ window.OUVRIERS = [
     "matricule": "1746",
     "nom": "LIENARD",
     "prenom": "JONATHAN",
+    "naissance": "1998-07-11",
     "entree": "2025-02-10",
     "qualif": "5",
     "fonction": "Maçons"
@@ -476,6 +535,7 @@ window.OUVRIERS = [
     "matricule": "1747",
     "nom": "LAENEN",
     "prenom": "KEVIN",
+    "naissance": "1991-11-11",
     "entree": "2025-02-10",
     "qualif": "1",
     "fonction": "Maçons"
@@ -484,6 +544,7 @@ window.OUVRIERS = [
     "matricule": "1749",
     "nom": "DELCOURT",
     "prenom": "VINCENT",
+    "naissance": "1992-04-24",
     "entree": "2025-02-11",
     "qualif": "6",
     "fonction": "Grutiers"
@@ -492,6 +553,7 @@ window.OUVRIERS = [
     "matricule": "1750",
     "nom": "PARFONRY",
     "prenom": "THIBAUT",
+    "naissance": "1995-12-28",
     "entree": "2025-03-19",
     "qualif": "6",
     "fonction": "Electricien"
@@ -500,6 +562,7 @@ window.OUVRIERS = [
     "matricule": "1751",
     "nom": "POLIMENI",
     "prenom": "GIOVANNI",
+    "naissance": "1998-09-25",
     "entree": "2025-03-19",
     "qualif": "6",
     "fonction": "Maçons"
@@ -508,6 +571,7 @@ window.OUVRIERS = [
     "matricule": "1752",
     "nom": "LARIVERA",
     "prenom": "THOMAS",
+    "naissance": "1987-12-10",
     "entree": "2025-04-07",
     "qualif": "3",
     "fonction": "Maçons"
@@ -516,6 +580,7 @@ window.OUVRIERS = [
     "matricule": "1753",
     "nom": "HASSELIN",
     "prenom": "LEO",
+    "naissance": "2003-08-27",
     "entree": "2025-04-07",
     "qualif": "5",
     "fonction": "Maçons"
@@ -524,6 +589,7 @@ window.OUVRIERS = [
     "matricule": "1754",
     "nom": "DEMEZEL",
     "prenom": "MASSIMO",
+    "naissance": "2001-05-08",
     "entree": "2025-05-05",
     "qualif": "5",
     "fonction": "Maintenance"
@@ -532,6 +598,7 @@ window.OUVRIERS = [
     "matricule": "1755",
     "nom": "DE KLERCK",
     "prenom": "ADRIEN",
+    "naissance": "2004-07-31",
     "entree": "2025-08-05",
     "qualif": "99",
     "fonction": "PFI"
@@ -540,6 +607,7 @@ window.OUVRIERS = [
     "matricule": "1756",
     "nom": "BAMBISI",
     "prenom": "JEANCY",
+    "naissance": "2004-04-27",
     "entree": "2025-08-05",
     "qualif": "99",
     "fonction": "PFI"
@@ -548,6 +616,7 @@ window.OUVRIERS = [
     "matricule": "1757",
     "nom": "VILLAREALE",
     "prenom": "ROMAIN",
+    "naissance": "1991-12-29",
     "entree": "2025-09-01",
     "qualif": "2",
     "fonction": "Chauffeurs"
@@ -556,6 +625,7 @@ window.OUVRIERS = [
     "matricule": "1758",
     "nom": "KUBAN",
     "prenom": "ADRIEN",
+    "naissance": "1991-02-24",
     "entree": "2025-09-22",
     "qualif": "3",
     "fonction": "Maçons"
@@ -564,6 +634,7 @@ window.OUVRIERS = [
     "matricule": "1759",
     "nom": "HULIN",
     "prenom": "SEBASTIEN",
+    "naissance": "1992-02-29",
     "entree": "2025-10-01",
     "qualif": "3",
     "fonction": "Maçons"
